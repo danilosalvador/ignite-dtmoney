32a3d3248ab66c6c04b45bb4ec45469f109e5d58
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-09-02 20:23:20 -0300

    Estrutura visual - Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionContainer, TransactionTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+            <TransactionContainer>
+                <TransactionTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Projeto</td>
+                            <td>02/09/2024</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hamburger</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    - R$ 59,90
+                                </PriceHighlight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>02/09/2024</td>
+                        </tr>
+                    </tbody>
+                </TransactionTable>
+            </TransactionContainer>
         </div>
     )
 }
