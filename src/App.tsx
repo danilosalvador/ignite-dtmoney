fa1727753bb5f1235320a10c3885ac70f6bd7356
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-09-02 19:39:19 -0300

    Estrutura visual - Componente: Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { ThemeProvider } from "styled-components";
 
 import { defaultTheme } from "./styles/theme/default";
 import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <div>dasd</div>
+      <Transactions />
     </ThemeProvider>
   )
 }
