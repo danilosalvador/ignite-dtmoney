9a49f94d6f439189805ac3736ceb1980233b1785
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-09-05 16:09:21 -0300

    Estrutura visual - Criando radio button acessível

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
+
 import * as Dialog from '@radix-ui/react-dialog';
+import * as RadioGroup from '@radix-ui/react-radio-group';
 
 export const Overlay = styled(Dialog.Overlay)`
     position: fixed;
@@ -107,7 +109,7 @@ export const CloseButton = styled(Dialog.Close)`
     line-height: 0;
 `
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     gap: 1rem;
@@ -119,7 +121,7 @@ interface TransactionTypeButtonProps {
     variant: 'income' | 'outcome';
 }
 
-export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButtonProps>`
     cursor: pointer;
     
     display: flex;
@@ -137,6 +139,21 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
 
     svg {
         color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]};
+    }
+
+    &[data-state='unchecked']:hover {
+        transition: background-color 0.2s;
+        background-color: ${props => props.theme["gray-600"]};
+    }
+
+    &[data-state='checked'] {
+        transition: background-color 0.2s, color 0.2s;
+        background-color: ${props => props.variant === 'income' ? props.theme["green-500"] : props.theme["red-500"]};
+        
+        color: ${props => props.theme.white};
+        svg {
+            color: ${props => props.theme.white};
+        }
     }
 
 `
