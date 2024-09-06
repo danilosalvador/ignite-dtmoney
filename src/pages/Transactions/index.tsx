ff3fe36fe2cc02f209325a411c5cd7db45215140
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-09-06 17:53:37 -0300

    Conexão com a API - Formatando valores

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -3,6 +3,7 @@ import { SearchForm } from "./SearchForm";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { PriceHighlight, TransactionContainer, TransactionTable } from "./styles";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Transactions() {
     const { transactions } = useTransactions();
@@ -20,11 +21,12 @@ export function Transactions() {
                                 <td width="50%">{transaction.description}</td>
                                 <td>
                                     <PriceHighlight variant={transaction.type}>
-                                        {transaction.price}
+                                        {transaction.type === 'outcome' && '- '}
+                                        {priceFormatter.format(transaction.price)}
                                     </PriceHighlight>
                                 </td>
                                 <td>{transaction.category}</td>
-                                <td>{transaction.createdAt}</td>
+                                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                             </tr>
                         ))}
                     </tbody>
